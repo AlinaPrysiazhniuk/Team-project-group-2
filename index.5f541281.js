f2cea0621726f33a9b6e5b15b4eccb3b57b1b1a7
AlinaPrysiazhniuk <114912643+AlinaPrysiazhniuk@users.noreply.github.com>
2022-12-28 11:07:42 +0000

    Deploying to gh-pages from @ AlinaPrysiazhniuk/Team-project-group-2@d86057f1dbcfd2d05d47e00bbe21a412bbef9312 🚀

---
+++ index.5f541281.js
@@ -0,0 +1,2 @@
+let e=document.querySelector("[id='buy-modal-desktop-tel']"),t=document.querySelector("[id='buy-modal-desktop-name']"),d=document.querySelector("[id='buy-modal-desktop-button']");function n(){d.disabled=!e.value||!t.value}e.addEventListener("input",(function(){n()})),t.addEventListener("input",(function(){n()}));
+//# sourceMappingURL=index.5f541281.js.map
