2c9bd7e6d33a8f0a1fd1dfb79ed493498aefa489
AlinaPrysiazhniuk <114912643+AlinaPrysiazhniuk@users.noreply.github.com>
2022-12-28 09:55:39 +0000

    Deploying to gh-pages from @ AlinaPrysiazhniuk/Team-project-group-2@2319ab0004bd01f24a0058a96a8635adf89eb2ae 🚀

---
+++ index.8809d47a.js
@@ -0,0 +1,2 @@
+(()=>{const o={openModalBtn:document.querySelector("[id='buy-now-mob-modal-open']"),closeModalBtn:document.querySelector("[id='buy-mob-modal-close']"),modal:document.querySelector("[id='buy-mob-modal']")};function e(){o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)})();
+//# sourceMappingURL=index.8809d47a.js.map
---
+++ index.ed7a2d75.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector("[id='buy-now-mob-modal-open']"),closeModalBtn:document.querySelector("[id='buy-mob-modal-close']"),modal:document.querySelector("[id='buy-mob-modal']")};function e(){o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.ed7a2d75.js.map
