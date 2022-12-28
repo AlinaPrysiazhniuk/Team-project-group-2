4ea4fbc5d6c4ed359153fc8b574a46d97a857ed8
Alina Prysiazhnuyk <alnkaprisiazhnuyk@gmail.com>
2022-12-28 03:01:18 +0100

    Update files

--- src/js/disabled/buy-modal-desktop-disabled.js
+++ src/js/disabled/buy-modal-desktop-disabled.js
@@ -1,15 +1,15 @@
 // buy-now-modal
 let phone = document.querySelector("[id='buy-modal-desktop-tel']");
-let user = document.querySelector("[id='buy-modal-desktop-name']");
+let name3 = document.querySelector("[id='buy-modal-desktop-name']");
 let button = document.querySelector("[id='buy-modal-desktop-button']");
 phone.addEventListener('input', function() {
   validate();
 });
 
-user.addEventListener('input', function() {
+name3.addEventListener('input', function() {
  validate();
 });
 
 function validate(){
-button.disabled = !phone.value || !user.value;
+button.disabled = !phone.value || !name3.value;
 }
