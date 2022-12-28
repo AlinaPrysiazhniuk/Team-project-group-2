ae07170abf154994ce0d79f8eeb35b3d25edbdd3
AlinaPrysiazhniuk <114912643+AlinaPrysiazhniuk@users.noreply.github.com>
2022-12-28 01:51:12 +0000

    Deploying to gh-pages from @ AlinaPrysiazhniuk/Team-project-group-2@bcb62e2338a7e1126b8671fadd81bbfbbd5c4ffe 🚀

---
+++ index.80e6252c.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("[id='buy-modal-desktop-tel']"),t=document.querySelector("[id='buy-modal-desktop-name']"),n=document.querySelector("[id='buy-modal-desktop-button']");function d(){n.disabled=!e.value||!t.value}e.addEventListener("input",(function(){d()})),t.addEventListener("input",(function(){d()}))}();
+//# sourceMappingURL=index.80e6252c.js.map
