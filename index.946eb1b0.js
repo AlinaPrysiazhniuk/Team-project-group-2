ec157d20f3a22f8bb2696e0dd4b1b6a3c5c1e096
AlinaPrysiazhniuk <114912643+AlinaPrysiazhniuk@users.noreply.github.com>
2023-01-01 17:27:52 +0000

    Deploying to gh-pages from @ AlinaPrysiazhniuk/Team-project-group-2@ffe59d9a9cdf74c20d3a00f5239d548f9621ec6e 🚀

--- index.946eb1b0.js
+++
@@ -1,2 +0,0 @@
-$(".autoplay").slick({slidesToShow:1,slidesToScroll:1,arrows:!1,fade:!0,dots:!1,infinite:!0,asNavFor:".single-item"}),$(".single-item").slick({slidesToShow:1,slidesToScroll:1,arrows:!1,fade:!0,dots:!0,infinite:!0,adaptiveHeight:!0,speed:300,asNavFor:".autoplay"});
-//# sourceMappingURL=index.946eb1b0.js.map
---
+++ index.c549f039.js
@@ -0,0 +1,2 @@
+$(".autoplay").slick({slidesToShow:1,slidesToScroll:1,arrows:!1,fade:!0,dots:!1,infinite:!0}),$(".single-item").slick({slidesToShow:1,slidesToScroll:1,arrows:!1,fade:!0,dots:!0,infinite:!0,adaptiveHeight:!0,speed:300,asNavFor:".autoplay"});
+//# sourceMappingURL=index.c549f039.js.map
